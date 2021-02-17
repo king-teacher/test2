ee558f0bc17835ff05854860a17c92aacbc4eb68
吕布 <king.com>
2021-02-17 16:29:46 +0800

    demo2

---
+++ src/main/java/cn/tx/demo/Demo2.java
@@ -0,0 +1,20 @@
+/**
+ * FileName:Demo2
+ * Author:Administrator
+ * Date:2021/2/17 16:28
+ * <autor>  <time>  <version>  <desc>
+ * 作者姓名  修改时间   版本号     描述
+ */
+package cn.tx.demo;
+
+public class Demo2 {
+
+	public void test1() {
+
+		System.out.println("hello");
+	}
+
+	public void test2() {
+
+	}
+}
