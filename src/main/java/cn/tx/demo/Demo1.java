8b1b99e5fef7f58881d9511c6e8a0c50bc011c45
张飞 <king.com>
2021-02-17 16:18:34 +0800

    demo1

---
+++ src/main/java/cn/tx/demo/Demo1.java
@@ -0,0 +1,14 @@
+/**
+ * FileName:Demo1
+ * Author:Administrator
+ * Date:2021/2/17 16:18
+ * <autor>  <time>  <version>  <desc>
+ * 作者姓名  修改时间   版本号     描述
+ */
+package cn.tx.demo;
+
+public class Demo1 {
+	public static void main(String[] args) {
+		System.out.println("hello");
+	}
+}
